bc309001e33552a3c83887d012299d5d201f1b7f
mdweb-dev <mdweb.com.ua@gmail.com>
2024-12-09 19:54:19 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,20 @@
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Test task</title>
+    <?php include('components/head/_head.php') ?>
+</head>
+<body class="home">
+    <div class="container">
+        <ul>
+            <li>
+                <a href="pages/test.php">home</a>
+            </li>
+        </ul>
+    </div>
+
+    <?php include('components/footer/_footer.php') ?>
+</body>
+</html>

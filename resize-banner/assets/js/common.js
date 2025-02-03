795acf055d1a0fa538ff72b946e3f3d361e2e9d4
mdweb-dev <mdweb.com.ua@gmail.com>
2025-02-03 16:58:49 +0200

    add banner

---
+++ resize-banner/assets/js/common.js
@@ -0,0 +1,14 @@
+/******/ (() => { // webpackBootstrap
+/*!*******************************!*\
+  !*** ./src/scripts/common.js ***!
+  \*******************************/
+window.addEventListener("DOMContentLoaded", function () {});
+
+//If you really need Jquery
+/*
+$(document).ready(function(){
+
+})
+ */
+/******/ })()
+;
